a8c7dd0a78ddff7119bd8257933e65a4df0ab7e6
fairhurt <fairhurt@oregonstate.edu>
2020-05-23 12:52:01 -0700

    Added modal button functionality, and creating the user inputted url from a search query

--- public/index.js
+++ public/index.js
@@ -8,17 +8,59 @@ closeButton[0].addEventListener('click', closeModal);
 var cancelButton= document.getElementsByClassName("modal-cancel-button");
 cancelButton[0].addEventListener('click', closeModal);
 
+var acceptButton= document.getElementsByClassName("modal-accept-button");
+acceptButton[0].addEventListener('click', acceptModal);
+
+var searchButton = document.getElementById("navbar-search-button");
+var url= "http://dnd5eapi.co/api/monsters/";
+
 initiativeButton.onclick = function() {
   modal.style.display = "block";
-   console.log("button clicked");
-   document.getElementById("modal-text-input").value= "";
-   document.getElementById("modal-attribution-input").value= "";
+  console.log("button clicked");
+  document.getElementById("modal-text-input").value= "";
+  document.getElementById("modal-number-input").value= "";
 
 }
 
+searchButton.onclick = function() {
+  var searchTerm = document.getElementById("navbar-search-input").value;
+  var newUrl=url+searchTerm;
+
+  console.log("the url is: "+ newUrl);
+  document.getElementById("navbar-search-input").value="";
+
+}
+
+
+function getData(data){
+  console.log(data);
+
+}
 // Closes the modal button
 function closeModal(){
   modal.style.display= "none";
   document.getElementById("modal-text-input").value= "";
-  document.getElementById("modal-attribution-input").value= "";
+  document.getElementById("modal-number-input").value= "";
+}
+//this function will accept the modal inputs, and check them for incorrect inputs
+function acceptModal(){
+  var textInput = document.getElementById("modal-text-input").value;
+  var numberInput=document.getElementById("modal-number-input").value;
+  modal.style.display= "none";
+
+  if(textInput===""){
+    prompt();
+  }else if(numberInput===""){
+    prompt();
+  }else{
+// this will call the function that handles the initiative rolling/displaying of the creatures
+  }
+
+}
+//prompts user for correct inputs
+function prompt(){
+  alert("Enter values into the fields");
+  modal.style.display = "block";
+  console.log("button clicked");
+
 }
