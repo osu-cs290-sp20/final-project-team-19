11e36f8aec3a13c4d345107fdca806bfcbc54c99
fairhurt <55992872+fairhurt@users.noreply.github.com>
2020-06-07 15:19:04 -0700

    still working on search bar functionality

--- public/index.js
+++ public/index.js
@@ -31,36 +31,54 @@ initiativeButton.onclick = function() {
 
 }
 
+//___________________________________________
 //This function takes the users input from the search bar and puts the stat block in place
 searchButton.onclick = function() {
+
   var textInput = document.getElementById("navbar-search-input").value;
+  console.log("here is the users search query input: ", textInput);
   var newUrl= createUrl(textInput, url);
-  document.getElementById("navbar-search-input").value="";
-  const creatureData = fetch(newUrl)
-  .then(function(response) {
-    if (response.status !== 200) {
-      console.log('Looks like there was a problem. Status Code: ' +
-      response.status);
-    inputPrompt(textInput);
-      return;
-    }
-    // Examine the text in the response
-    response.json().then(function(data) {
+//get the search query from the drop down menu
+  var searchQuery = document.getElementById("search-query");
+// access the option selected by the user
+  var searchField=(searchQuery.options[searchQuery.selectedIndex].value);
+//check the users input and generate the proper link for the specified field
+    if(searchField =="Monsters"){
+      var creatureUrl=createUrl(textInput, url);
+      const creatureData = fetch(creatureUrl)
+      .then(function(response) {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+          inputPrompt(textInput);
+          return;
+        }
+        // Examine the text in the response
+        response.json().then(function(data) {
 
 
-  generateCreatureStatBlock(data)
+          generateCreatureStatBlock(data);
 
+        });
+      }
+    )
+    .then(data => apiData = data)
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
     });
+      console.log("Here is the creatureURl: ", creatureUrl);
+  }else if(searchField== "Equipment"){
+
+    console.log("here is the users search query input: ", textInput);
+  }else if(searchField== "Spells"){
+
+    console.log("here is the users search query input: ", textInput);
+  }else{
+    var textInput=searchField;
+    inputPrompt(textInput);
   }
-)
-.then(data => apiData = data)
-.catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
-
-
 }
-
+//___________________________________________
 
 
 function getData(data){
@@ -86,8 +104,8 @@ function closeModal(){
 
 //this function will accept the modal inputs, and check them for incorrect inputs
 function acceptModal(){
-error=0;
-var apiData;
+  error=0;
+  var apiData;
   var textInput = document.getElementById("modal-text-input").value;
   var numberInput=document.getElementById("modal-number-input").value;
 
@@ -107,15 +125,15 @@ var apiData;
 
     modalBackdrop.classList.add('hidden');
     searchCreatureModal.classList.add('hidden');
-//________________________________________________________
-//working api fetch code
+    //________________________________________________________
+    //working api fetch code
 
     const creatureData = fetch(apiUrl)
     .then(function(response) {
       if (response.status !== 200) {
         console.log('Looks like there was a problem. Status Code: ' +
         response.status);
-      inputPrompt(textInput);
+        inputPrompt(textInput);
         return;
       }
       // Examine the text in the response
@@ -129,8 +147,8 @@ var apiData;
   )
   .then(data => apiData = data)
   .catch(function(err) {
-      console.log('Fetch Error :-S', err);
-    });
+    console.log('Fetch Error :-S', err);
+  });
 
 
 
@@ -142,15 +160,15 @@ var apiData;
 //This function will take the users input, remove the spaces and make it lower case
 //________________________________________________________
 function createUrl(textInput, url){
-// make input lowercase
-var textInputLower = textInput.toLowerCase();
+  // make input lowercase
+  var textInputLower = textInput.toLowerCase();
 
-//replace all of the spaces with dashes
-var newUrl = textInputLower.replace(/ /g, "-");
-//combine the global url to the search query
-var finalUrl= url+newUrl;
-//return the final url to the fetch statement
-return finalUrl;
+  //replace all of the spaces with dashes
+  var newUrl = textInputLower.replace(/ /g, "-");
+  //combine the global url to the search query
+  var finalUrl= url+newUrl;
+  //return the final url to the fetch statement
+  return finalUrl;
 
 }
 
@@ -159,42 +177,42 @@ return finalUrl;
 //This function will take the json data from the generateCreatures and show the important information
 //________________________________________________________
 function generateCreatures(num, data){
-// var creatureArray = JSON.parse(data);
-console.log(Object.keys(data));
-console.log(data["hit_dice"]);
-var hitPoints=data["hit_points"];
-var armorClass=data["armor_class"];
-var creatureName = data["name"];
-console.log("HERE ARE THE HIT POINTS FROM THE OBJECT: ", hitPoints);
+  // var creatureArray = JSON.parse(data);
+  console.log(Object.keys(data));
+  console.log(data["hit_dice"]);
+  var hitPoints=data["hit_points"];
+  var armorClass=data["armor_class"];
+  var creatureName = data["name"];
+  console.log("HERE ARE THE HIT POINTS FROM THE OBJECT: ", hitPoints);
   // console.log("Here is the data in the creature data function");
   // console.log("here is the data", data);
   //for(var i=0; i<num; i++){
-//var initative= Math.floor((Math.random() * 20) + 1);
-    // creatureArray[i]=initative;
+  //var initative= Math.floor((Math.random() * 20) + 1);
+  // creatureArray[i]=initative;
   //}
   var dex = data["dexterity"];
   var creatureContextArray = Array(num);
 
-//Creating Monster Stat block
-generateCreatureStatBlock(data);
-//done with that
+  //Creating Monster Stat block
+  generateCreatureStatBlock(data);
+  //done with that
 
   for(var i=0; i<num; i++){
-   var random = Math.floor((Math.random()*20)+1);
-   var init =Math.floor(random + ((dex-10)/2));
+    var random = Math.floor((Math.random()*20)+1);
+    var init =Math.floor(random + ((dex-10)/2));
 
-   //stupid name that can be taken out
-   var sillyname=generateName();
+    //stupid name that can be taken out
+    var sillyname=generateName();
 
-   var creatureContext = {
-    initiative: init,
-    armor_class: armorClass,
-    hit_points: hitPoints,
-    name: creatureName,
-    sname:sillyname
+    var creatureContext = {
+      initiative: init,
+      armor_class: armorClass,
+      hit_points: hitPoints,
+      name: creatureName,
+      sname:sillyname
 
-   }
-   creatureContextArray[i] = creatureContext;
+    }
+    creatureContextArray[i] = creatureContext;
   }
 
   creatureContextArray.sort(function(a,b) {
@@ -203,17 +221,17 @@ generateCreatureStatBlock(data);
   creatureContextArray.reverse();
 
   for(var i=0; i<num; i++){
-  var creatureHtml = Handlebars.templates.creatureTemplate(creatureContextArray[i]);
+    var creatureHtml = Handlebars.templates.creatureTemplate(creatureContextArray[i]);
 
-  var creatureContainer = document.querySelector('main.creature-container');
-  creatureContainer.insertAdjacentHTML('beforeend', creatureHtml);
+    var creatureContainer = document.querySelector('main.creature-container');
+    creatureContainer.insertAdjacentHTML('beforeend', creatureHtml);
   }
 }
 
 
 
 function inputPrompt(textInput){
-  alert("Requested creature: "+ textInput + " was not found in the dnd5eapi");
+  alert("Your request for: "+ textInput + " was not found in the dnd5eapi");
   modal.style.display = "block";
 
 }
@@ -226,17 +244,17 @@ Random Name Generator for Javascript
 */
 
 function capFirst(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function getRandomInt(min, max) {
-  	return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function generateName(){
-	var name1 = ["Jack","Eragon","Samwise","Frodo","Gandalf","Gimli","Aragorn","Legolas","Zaque","Zacckk","Zacck","ZACK","Zakk","Zak","Rob Hess","Liam","Noah","William","James","Oliver","Benjamin","Elijah","Lucas","Mason","Logan","Emma","Olivia","Ava","Isabella","Sophia","Charlotte","Mia","Amelia","Harper","Evelyn","Piers Borngasser","Ian Bailey","Tyson Fairhurst","Ethan Hunter","Jeff","Zach","Zack","Zacc","Zachary","Bob","Joe","Brian","THE DUDE","Your Worst Enemy","Ezekiel","Michael","David","John","James","Robert","Mark","William","Richard","Thomas","Jeffrey","Steven","Joseph","Timothy","Kevin","Scott","Brian","Charles","Paul","Daniel","Christopher","Kenneth","Anthony","Gregory","Ronald","Donald","Gary","Stephen","Eric","Edward","Douglas","Todd","Patrick","Keith","Larry","Matthew","Terry","Andrew","Dennis","Randy","Jerry","Peter","Frank","Craig","Raymond","Jeffery","Bruce","Rodney","Mike","Roger","Tony","Ricky","Steve","Jeff","Troy","Alan","Carl","Danny","Russell","Chris","Bryan","Gerald","Wayne","Joe","Randall","Lawrence","Dale","Phillip","Johnny","Vincent","Martin","Bradley","Billy","Glenn","Shawn","Jonathan","Jimmy","Sean","Curtis","Barry","Bobby","Walter","Jon","Philip","Samuel","Jay","Jason","Dean","Jose","Tim","Roy","Willie","Arthur","Darryl","Henry","Darrell","Allen","Victor","Harold","Greg"]
-	var name = capFirst(name1[getRandomInt(0, name1.length)]);
-	return name;
+  var name1 = ["Jack","Eragon","Samwise","Frodo","Gandalf","Gimli","Aragorn","Legolas","Zaque","Zacckk","Zacck","ZACK","Zakk","Zak","Rob Hess","Liam","Noah","William","James","Oliver","Benjamin","Elijah","Lucas","Mason","Logan","Emma","Olivia","Ava","Isabella","Sophia","Charlotte","Mia","Amelia","Harper","Evelyn","Piers Borngasser","Ian Bailey","Tyson Fairhurst","Ethan Hunter","Jeff","Zach","Zack","Zacc","Zachary","Bob","Joe","Brian","THE DUDE","Your Worst Enemy","Ezekiel","Michael","David","John","James","Robert","Mark","William","Richard","Thomas","Jeffrey","Steven","Joseph","Timothy","Kevin","Scott","Brian","Charles","Paul","Daniel","Christopher","Kenneth","Anthony","Gregory","Ronald","Donald","Gary","Stephen","Eric","Edward","Douglas","Todd","Patrick","Keith","Larry","Matthew","Terry","Andrew","Dennis","Randy","Jerry","Peter","Frank","Craig","Raymond","Jeffery","Bruce","Rodney","Mike","Roger","Tony","Ricky","Steve","Jeff","Troy","Alan","Carl","Danny","Russell","Chris","Bryan","Gerald","Wayne","Joe","Randall","Lawrence","Dale","Phillip","Johnny","Vincent","Martin","Bradley","Billy","Glenn","Shawn","Jonathan","Jimmy","Sean","Curtis","Barry","Bobby","Walter","Jon","Philip","Samuel","Jay","Jason","Dean","Jose","Tim","Roy","Willie","Arthur","Darryl","Henry","Darrell","Allen","Victor","Harold","Greg"]
+  var name = capFirst(name1[getRandomInt(0, name1.length)]);
+  return name;
 
 }
 
@@ -272,34 +290,34 @@ function generateCreatureStatBlock(data) {
   var act = data["actions"];
   var lact = data["legendary_actions"];
 
- console.log(lact);
- console.log(act);
- console.log(con_immune);
- if (prof.length == 0){
+  console.log(lact);
+  console.log(act);
+  console.log(con_immune);
+  if (prof.length == 0){
     prof=[{name:"N/A"}];
- }
- if (vuls.length == 0){
+  }
+  if (vuls.length == 0){
     console.log("in vuls");
     vuls= "N/A";
- }
- if(resist.length == 0){
+  }
+  if(resist.length == 0){
     resist="N/A";
   }
-if (immune.length == 0){
+  if (immune.length == 0){
     immune="N/A";
-}
+  }
   if (con_immune.length == 0){
     con_immune=[{name:"N/A"}];
- }
- if (sa.length == 0){
+  }
+  if (sa.length == 0){
     sa=[{name:"N/A"}];
- }
- if (act.length == 0){
+  }
+  if (act.length == 0){
     act=[{name:"N/A"}];
- }
+  }
   if (lact==null || lact.length == 0){
     lact=[{name:"N/A"}];
- }
+  }
 
   var creatureInfoContext = {
     name: name,
@@ -318,22 +336,22 @@ if (immune.length == 0){
     sa: sa,
     act: act,
     lact: lact,
- }
+  }
 
- //checking if this is a new creature or a copy of one already on the sheet
- var temp=1;
- for(var i =0; i<priorInfo.length;i++){
+  //checking if this is a new creature or a copy of one already on the sheet
+  var temp=1;
+  for(var i =0; i<priorInfo.length;i++){
     if(JSON.stringify(priorInfo[i])==JSON.stringify(creatureInfoContext)){
-        temp=0;
-	}
- }
+      temp=0;
+    }
+  }
 
- if(temp==1){
-      var creatureInfoHtml = Handlebars.templates.creatureInfoTemplate(creatureInfoContext);
+  if(temp==1){
+    var creatureInfoHtml = Handlebars.templates.creatureInfoTemplate(creatureInfoContext);
 
-      var creatureContainer = document.querySelector('main.creature-info-container');
-      creatureContainer.insertAdjacentHTML('beforeend', creatureInfoHtml);
-      priorInfo[priorInfo.length]=creatureInfoContext;
+    var creatureContainer = document.querySelector('main.creature-info-container');
+    creatureContainer.insertAdjacentHTML('beforeend', creatureInfoHtml);
+    priorInfo[priorInfo.length]=creatureInfoContext;
   }
 }
 //functions for handling the drop down menu
