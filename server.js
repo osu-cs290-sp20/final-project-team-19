401a0362e78db09f4516d6df7b8efd6cabb17ea5
borngasp <56008554+borngasp@users.noreply.github.com>
2020-06-09 18:11:27 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 var path = require('path');
 var fs= require('fs');
 var express = require('express');
-
+var bp = require('body-parser');
 var exphbs = require('express-handlebars');
 
 var app = express();
 var http = require("http");
 
+var characterData = require("./characterData");
+
+app.use(bp.json());
+
 app.engine('handlebars',exphbs({defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
 
 
 
@@ -18,7 +23,9 @@ var logger = require('./logger');
 
 app.use(logger);
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(/*__dirname + */"public"));
+
+
 
 app.get('/', function(req, res, next){
   res.status(200).render('index');
@@ -35,11 +42,40 @@ app.get('/about',function(req, res, next){
 app.get('/spells',function(req, res, next){
   res.status(200).render('spells');
 });
-app.get('/api/spells/:name',function(req, res, next){
-  var name = req.params.name;
-  res.status(200).render('spells');
+
+app.get('/characters',function(req, res, next){
+  res.status(200).render('characters');
 });
 
+app.post("/characters/newCharacter", function(req,res,next) {
+  console.log("== req.body:", req.body);
+  console.log("  -- req.body.url:", req.body.name);
+  console.log("  -- req.body.caption:", req.body.dex);
+  var test = req.body;
+
+  characterData.characters.push({
+    name: req.body.name,
+    str: req.body.str,
+    dex: req.body.dex,
+    con: req.body.con,
+    int: req.body.int,
+    wis: req.body.wis,
+    cha: req.body.cha,
+    ac: req.body.ac,
+    hp: req.body.hp,
+    ms: req.body.ms
+  });
+
+  let data = JSON.stringify(test);
+  fs.writeFileSync('characterData.json', data);
+
+  res.status(200).send("Charcter done");
+
+});
+
+app.get('/:spellName',function(req, res, next){
+  res.status(200).render('spellInfo');
+});
 
 app.get('*',function(req, res, next){
   res.status(200).render('404');
