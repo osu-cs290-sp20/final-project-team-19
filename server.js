33a47045f3b45a8f5dc6fc503083633a6aeae0a5
fairhurt <55992872+fairhurt@users.noreply.github.com>
2020-06-10 12:53:26 -0700

    CHARACTER WRITE TO FILE IS WORKING YAHHHH BUDDY

--- server.js
+++ server.js
@@ -53,26 +53,91 @@ app.get('/characters',function(req, res, next){
 });
 
 app.post("/characters/newCharacter", function(req,res,next) {
-  console.log("== req.body:", req.body);
-  console.log("  -- req.body.url:", req.body.name);
-  console.log("  -- req.body.caption:", req.body.dex);
-  var test = req.body;
+  var obj ={
 
-  characterData.characters.push({
-    name: req.body.name,
-    str: req.body.str,
-    dex: req.body.dex,
-    con: req.body.con,
-    int: req.body.int,
-    wis: req.body.wis,
-    cha: req.body.cha,
-    ac: req.body.ac,
-    hp: req.body.hp,
-    ms: req.body.ms
+    characters: []
+  };
+
+  // obj.characters.push({
+  //   name: req.body.name,
+  //   str: req.body.str,
+  //   dex: req.body.dex,
+  //   con: req.body.con,
+  //   int: req.body.int,
+  //   wis: req.body.wis,
+  //   cha: req.body.cha,
+  //   ac: req.body.ac,
+  //   hp: req.body.hp,
+  //   ms: req.body.ms
+  // })
+  var json = JSON.stringify(obj);
+
+  fs.readFile('characterData.json', 'utf8', function readFileCallback(err, data){
+    if (err){
+      console.log(err);
+    } else {
+      obj = JSON.parse(data); //now it an object
+      obj.characters.push({
+        name: req.body.name,
+        str: req.body.str,
+        dex: req.body.dex,
+        con: req.body.con,
+        int: req.body.int,
+        wis: req.body.wis,
+        cha: req.body.cha,
+        ac: req.body.ac,
+        hp: req.body.hp,
+        ms: req.body.ms
+      }); //add some data
+      json = JSON.stringify(obj, null, 2); //convert it back to json
+      fs.writeFile('characterData.json', json, 'utf8', function(err){
+    if(err) throw err;
+  }); // write it back
+    }
   });
 
-  let data = JSON.stringify(test);
-  fs.writeFileSync('characterData.json', data);
+
+
+
+
+  //+++++++++++++++++++++++++++++++++
+  // const characterData ={
+  //   name: req.body.name,
+  //   str: req.body.str,
+  //   dex: req.body.dex,
+  //   con: req.body.con,
+  //   int: req.body.int,
+  //   wis: req.body.wis,
+  //   cha: req.body.cha,
+  //   ac: req.body.ac,
+  //   hp: req.body.hp,
+  //   ms: req.body.ms
+
+  //}
+  // const characterDataString = JSON.stringify(characterData, null ,2);
+  // fs.writeFileSync('characterData.json',characterDataString);
+  //++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+  // console.log("== req.body:", req.body);
+  // console.log("  -- req.body.url:", req.body.name);
+  // console.log("  -- req.body.caption:", req.body.dex);
+  // var test = req.body;
+  //
+  // characterData.characters.push({
+  // name: req.body.name,
+  // str: req.body.str,
+  // dex: req.body.dex,
+  // con: req.body.con,
+  // int: req.body.int,
+  // wis: req.body.wis,
+  // cha: req.body.cha,
+  // ac: req.body.ac,
+  // hp: req.body.hp,
+  // ms: req.body.ms
+  // });
+  //
+  // let data = JSON.stringify(test);
+  // fs.writeFileSync('characterData.json', data);
 
   res.status(200).send("Charcter done");
 
@@ -82,7 +147,7 @@ app.get('/:spellName',function(req, res, next){
   var name = req.params.spellName;
   var spells= spellStuff.spell_check(name);
   if(spells) {
-      res.status(200).render('spellInfo');
+    res.status(200).render('spellInfo');
   }else{
     next();
   }
