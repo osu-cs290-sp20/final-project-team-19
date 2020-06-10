2c81780637709c46da4b8b42598daba021d39a18
baileyia <56174844+baileyia@users.noreply.github.com>
2020-06-09 23:36:05 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+//import {spell_check} from './spellStuff.js'
+
 var path = require('path');
 var fs= require('fs');
 var express = require('express');
 var bp = require('body-parser');
 var exphbs = require('express-handlebars');
+const fetch = require("node-fetch");
+var spellStuff= require("./spellStuff.js");
+
 
 var app = express();
 var http = require("http");
@@ -48,11 +53,12 @@ app.get('/characters',function(req, res, next){
 });
 
 app.post("/characters/newCharacter", function(req,res,next) {
-  // console.log("== req.body:", req.body);
-  // console.log("  -- req.body.url:", req.body.name);
-  // console.log("  -- req.body.caption:", req.body.dex);
-  // var test = req.body;
-  var character= {
+  console.log("== req.body:", req.body);
+  console.log("  -- req.body.url:", req.body.name);
+  console.log("  -- req.body.caption:", req.body.dex);
+  var test = req.body;
+
+  characterData.characters.push({
     name: req.body.name,
     str: req.body.str,
     dex: req.body.dex,
@@ -63,46 +69,24 @@ app.post("/characters/newCharacter", function(req,res,next) {
     ac: req.body.ac,
     hp: req.body.hp,
     ms: req.body.ms
-  }
+  });
 
-const characterString = JSON.stringify(character, null, 2);
-
-fs.writeFileSync('./characterData.json', characterString);
-
-  // fs.readFile('characterData.json', 'utf-8', function(err, data) {
-  //   if (err) throw err
-  //
-  //   var arrayOfObjects = JSON.parse(data);
-  //   arrayOfObjects.characterData.characters.push({
-  //     name: req.body.name,
-  //     str: req.body.str,
-  //     dex: req.body.dex,
-  //     con: req.body.con,
-  //     int: req.body.int,
-  //     wis: req.body.wis,
-  //     cha: req.body.cha,
-  //     ac: req.body.ac,
-  //     hp: req.body.hp,
-  //     ms: req.body.ms
-  //   });
-  //   fs.writeFile('characterData.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
-  //     if (err) throw err
-  //     console.log('Done!');
-  //   })
-  // })
-
-
-
-
-  // let data = JSON.stringify(test);
-  // fs.writeFileSync('characterData.json', data);
+  let data = JSON.stringify(test);
+  fs.writeFileSync('characterData.json', data);
 
   res.status(200).send("Charcter done");
 
 });
 
 app.get('/:spellName',function(req, res, next){
-  res.status(200).render('spellInfo');
+  var name = req.params.spellName;
+  var spells= spellStuff.spell_check(name);
+  if(spells) {
+      res.status(200).render('spellInfo');
+  }else{
+    next();
+  }
+  
 });
 
 app.get('*',function(req, res, next){
@@ -112,3 +96,5 @@ app.get('*',function(req, res, next){
 app.listen(3000, function(){
   console.log("The server is listening on port 3000");
 });
+
+
